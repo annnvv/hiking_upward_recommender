77eec89a2d6e8bb2ec810e751a56f7f28f8c8092
annnvv <13822260+annnvv@users.noreply.github.com>
2022-04-01 20:51:16 -0400

    initial work on streamlit app

---
+++ app/streamlit_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title("Hiking Upward Recommender")
+
+try:
+    url = st.text_input("Hiking Upward Hike URL")
+except ValueError: 
+    print("URL must start with https://www.hikingupward.com/")
+
+num_recommend = st.radio("Choose the number of recommendations", (5, 10, 15))
+
+if url:
+
+    hike_df = pd.read_csv("app/data/hiking_upward_data.csv")
+
+    recommend_df = pd.read_csv("app/data/nearest_25_recommendations_for_each_hike.csv")
+
+    one_hike = hike_df[hike_df.hike_url == url]
+    st.write(one_hike)
+
+    recommend_df_subset = recommend_df[
+        recommend_df.hike_url == url
+    ].head(num_recommend)
+
+    recommend_df_subset.drop("hike_url", axis=1, inplace=True)
+
+    merged = pd.merge(
+        recommend_df_subset, hike_df, left_on="similar", right_on="hike_url", how="left"
+    )
+
+    merged.drop(["similar", "score"], axis=1, inplace=True)
+
+    st.dataframe(merged)
+
